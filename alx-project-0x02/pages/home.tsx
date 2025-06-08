58fa8fe3dfb6de2a6d050f0daadcc1fe99e42ebc
Obaro Awowede <akpobaroawowede@gmail.com>
2025-06-08 19:49:18 +0100

    done tasks 3

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,11 @@
+import Card from '../components/common/Card';
+
 export default function Home() {
     return(
     <div>
         <h1>This is our home page</h1>
+        <Card title="House Available" content="2 bedroom duplex available, 3 months payment upfront" />
+         <Card title="Not Available" content="This house is not available for rent" />
     </div>
     );
 }
